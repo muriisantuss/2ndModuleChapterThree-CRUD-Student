e59763bd3d632e1e791d741c9a400de78a1da888
muriisantuss <muriisantuss@gmail.com>
2024-10-13 00:51:59 -0300

    :sparkles: closes #5 feat: insert saveStudents function

--- js/script.js
+++ js/script.js
@@ -1,53 +1,85 @@
-let students  = [
-  {
-  id: 1,
-  name: "muri",
-  email: "m@gmail.com",
-  phone: "11 1111-1111",
-  course: 1,
-  period: 1,
-  }
-]
-let courses = [
+let students = [
   {
     id: 1,
-    name: "Línguas",
+    name: "muri",
+    email: "m@gmail.com",
+    phone: "11 1111-1111",
+    course: 1,
+    manha: false,
+    tarde: false,
+    noite: true
   }
 ];
-var periods = [
+var courses = [
   {
     id: 1,
-    name: "Tarde",
-  }
+    name: "Jornalismo",
+  },
+  {
+    id: 2,
+    name: "Qualidade",
+  },
+  {
+    id: 3,
+    name: "Ecônomia",
+  },
 ];
 
-loadStudents()
+loadStudents();
 
-function loadStudents(){
-  for(let get of students){
-    addNewRow(get)
+function saveStudents() {
+  console.log("certo");
+  let getStudents = {
+      id: students.length+1,
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      phone: document.getElementById("phone").value,
+      course: document.getElementById("course").value,
+      manha: document.getElementById("manha").checked,
+      tarde: document.getElementById("tarde").checked,
+      noite: document.getElementById("noite").checked,
+
+    }
+  addNewRow(getStudents);
+  students.push(getStudents);
+}
+
+function loadStudents() {
+  for (let get of students) {
+    addNewRow(get);
   }
 }
 
-function addNewRow(s){
-  var table = document.getElementById("studentTable")
-  const newRow = table.insertRow()
+function addNewRow(s) {
+  var table = document.getElementById("studentTable");
+  const newRow = table.insertRow();
 
-  let idNode = document.createTextNode(s.id)
-  newRow.insertCell().appendChild(idNode)
+  let idNode = document.createTextNode(s.id);
+  newRow.insertCell().appendChild(idNode);
 
-  let nameNode = document.createTextNode(s.name)
-  newRow.insertCell().appendChild(nameNode)
+  let nameNode = document.createTextNode(s.name);
+  newRow.insertCell().appendChild(nameNode);
 
-  let emailNode = document.createTextNode(s.email)
-  newRow.insertCell().appendChild(emailNode)
+  let emailNode = document.createTextNode(s.email);
+  newRow.insertCell().appendChild(emailNode);
 
-  let phoneNode = document.createTextNode(s.phone)
-  newRow.insertCell().appendChild(phoneNode)
+  let phoneNode = document.createTextNode(s.phone);
+  newRow.insertCell().appendChild(phoneNode);
 
-  let courseNode = document.createTextNode(courses[s.course - 1].name)
-  newRow.insertCell().appendChild(courseNode)
+  let courseNode = document.createTextNode(courses[s.course - 1].name);
+  newRow.insertCell().appendChild(courseNode);
 
-  let periodNode = document.createTextNode(periods[s.period - 1].name)
-  newRow.insertCell().appendChild(periodNode).name
+  var periods = ""
+  if (s.manha) {
+    periods = 'Manha';
+  }
+  if (s.tarde) {
+    periods += 'Tarde';
+  }
+  if (s.noite) {
+    periods += 'Noite';
+  }
+
+  cell = newRow.insertCell()
+  cell.innerHTML = periods;
 }
